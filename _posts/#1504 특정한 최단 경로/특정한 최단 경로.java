7847577046f7fb305e1bface0dd2a1a5e8aad712
sdchoi <85655562+sdchoi07@users.noreply.github.com>
2023-12-16 21:17:40 +0900

    [Gold IV 1504] Title: 특정한 최단 경로, Time: 680 ms, Memory: 70068 KB -NKLCBHub

--- _posts/#1504 특정한 최단 경로/특정한 최단 경로.java
+++ _posts/#1504 특정한 최단 경로/특정한 최단 경로.java
@@ -23,7 +23,7 @@ public class Main {
 		n = Integer.parseInt(st.nextToken());
 		e = Integer.parseInt(st.nextToken());
 		list = new ArrayList[n+1];
-		for(int i = 1 ;i <=n; i++) {
+		for(int i = 0 ;i <=n; i++) {
 			list[i] = new ArrayList<int[]>();
 		}
 		
@@ -32,13 +32,14 @@ public class Main {
 			int from = Integer.parseInt(st.nextToken());
 			int to = Integer.parseInt(st.nextToken());
 			int cost = Integer.parseInt(st.nextToken());
-			list[from].add(new int[]{to,cost});
+			list[from].add(new int[] {to,cost});
 			list[to].add(new int[] {from,cost});
 		}
+		
 		st = new StringTokenizer(br.readLine(), " ");
 		int frist = Integer.parseInt(st.nextToken());
 		int second = Integer.parseInt(st.nextToken());
-				
+		
 		bfs(1, frist, true);
 		bfs(frist, second, true);
 		bfs(second, n, true);
@@ -46,58 +47,54 @@ public class Main {
 		bfs(1, second, false);
 		bfs(second, frist, false);
 		bfs(frist, n, false);
-		if(resultFrist >= Integer.MAX_VALUE/4 && resultSecond >=Integer.MAX_VALUE/4) {
+		if(resultFrist >= Integer.MAX_VALUE/4 && resultSecond >= Integer.MAX_VALUE/4) {
 			System.out.println(-1);
 		}else {
 			System.out.println(Math.min(resultFrist, resultSecond));
 		}
 	}
 	static void bfs(int start, int end, boolean flag) {
-		nums = new int[n+1]; 
-		chk  = new boolean[n+1];
-		Arrays.fill(nums, Integer.MAX_VALUE/4);	
-		
 		if(start == end) {
 			return;
-		}else {
-			int result = 0;
-			Queue<int[]> q = new PriorityQueue<int[]>(new Comparator<int []>() {
-
-				@Override
-				public int compare(int[] o1, int[] o2) {
-					return o1[1]-o2[1];
-				}
-				
-			});
-			
-			q.add(new int[]{start,0});
-			while(!q.isEmpty()) {
-				int[] qOut = q.poll();
-				int node = qOut[0];
-				if(chk[node]) {
-					continue;
-				}
-				chk[node] = true;
-				for(int i = 0; i <list[node].size(); i++) {
-					int[] next = list[node].get(i);
-					if(next != null) {
-						if(nums[next[0]] > qOut[1] + next[1] ) {
-							
-							nums[next[0]] =qOut[1]+next[1];
-							q.add(new int[] {next[0], nums[next[0]] });
-						}
-					}
-				}
-				
-				
-			}
-			result = nums[end];
-			if(flag) {
-				resultFrist+=result;
-			}else {
-				resultSecond+=result;
-			}
 		}
 		
+		nums = new int[n+1];
+		Arrays.fill(nums, Integer.MAX_VALUE/4);
+		chk = new boolean[n+1];
+		
+		int result = 0;
+		Queue<int[]> q = new PriorityQueue<int[]>(new Comparator<int[]>() {
+
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				return o1[1]-o2[1];
+			}
+			
+		});
+		
+		q.add(new int[] {start,0});
+		while(!q.isEmpty()) {
+			int[] qOut = q.poll();
+			int node = qOut[0];
+			if(chk[node]) {
+				continue;
+			}
+			chk[node] = true;
+			
+			for(int i = 0 ;i < list[node].size(); i++) {
+				int[] next = list[node].get(i);
+				if(nums[next[0]] > qOut[1] + next[1]) {
+					nums[next[0]] = qOut[1] + next[1];
+					q.add(new int[] {next[0],nums[next[0]]});
+				}
+			}
+		}
+		result = nums[end];
+		if(flag) {
+			resultFrist +=result;
+		}else {
+			resultSecond +=result;
+		}
+        
 	}
 }
