bea1765f3e02829673b2215049b1e4f69fdfb6cc
sdchoi <85655562+sdchoi07@users.noreply.github.com>
2023-12-24 09:17:41 +0900

    [Gold IV 11657] Title: 타임머신, Time: 264 ms, Memory: 17088 KB -NKLCBHub

--- _posts/#11657 타임머신/타임머신.java
+++ _posts/#11657 타임머신/타임머신.java
@@ -2,17 +2,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
- class Bus {
-    int start,end,cost;
-
-
-
-    public Bus(int start, int end, int cost){
-        this.start=start;
-        this.end = end;
-        this.cost = cost;
-    }
-}
+ class Bus{
+     int start, end, value;
+             public Bus(int start, int end, int value){
+                this.start = start;
+                this.end = end;
+                this.value = value;
+             }
+ }
 public class Main {
 
     static int n, m, x;
@@ -23,50 +20,51 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        dist = new long[n+1];
         bus = new Bus[m];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[1] = 0;
-        for (int i = 0; i < m; i++) {
+        dist = new long[n+1];
+        Arrays.fill(dist , Integer.MAX_VALUE);
+        dist[1] =  0;
+        for(int i = 0 ;i < m; i++){
             st = new StringTokenizer(br.readLine(), " ");
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
-            int time = Integer.parseInt(st.nextToken());
-            bus[i] = new Bus(from, to, time);
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int value = Integer.parseInt(st.nextToken());
+            bus[i]  = new Bus(start, end, value);
         }
-        for (int i = 0; i < n - 1; i++) {
+
+        for(int i = 0 ;i < n-1; i++) {
+            for (int j = 0; j < m; j++) {
+                Bus b = bus[j];
+                int curStart = b.start;
+                int curEnd = b.end;
+                int curValue = b.value;
+                if (dist[curStart] != Integer.MAX_VALUE && dist[curEnd] > dist[curStart] + curValue) {
+                    dist[curEnd] = dist[curStart] + curValue;
+                }
+            }
+        }
+            boolean update = false;
             for(int j = 0 ;j < m; j++){
                 Bus b = bus[j];
-                int from = b.start;
-                int to = b.end;
-                int cost = b.cost;
-                if(dist[from] != Integer.MAX_VALUE && dist[to] > dist[from] + cost){
-                    dist[to] = dist[from] + cost;
+                int curStart = b.start;
+                int curEnd = b.end;
+                int curValue = b.value;
+                if(dist[curStart] != Integer.MAX_VALUE && dist[curEnd] > dist[curStart] +  curValue){
+                    update = true;
+                    break;
                 }
             }
-        }
-        boolean chk = false;
-        for(int j = 0 ;j < m; j++){
-            Bus b = bus[j];
-            int from = b.start;
-            int to = b.end;
-            int cost = b.cost;
-            if(dist[from] != Integer.MAX_VALUE && dist[to] > dist[from] + cost){
-               chk = true;
-               break;
-            }
-        }
-        if(!chk){
-            for(int i = 2; i<n+1; i++){
-                if(dist[i] != Integer.MAX_VALUE){
-                    System.out.println(dist[i]);
-                }else{
-                    System.out.println(-1);
+            if(!update){
+                for(int i = 2; i <=n;i++){
+                    if(dist[i] != Integer.MAX_VALUE){
+                        System.out.println(dist[i]);
+                    }else{
+                        System.out.println(-1);
+                    }
                 }
+            }else{
+                System.out.println(-1);
             }
-        }else {
-            System.out.println(-1);
-        }
     }
 
 }
