4f364fd65fb2ebe940d23246cd1c49ed8f070dcf
sdchoi <85655562+sdchoi07@users.noreply.github.com>
2024-03-29 10:30:30 +0900

    [Bronze II] Title: 소수 찾기, Time: 80 ms, Memory: 11556 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -1,33 +1,31 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int[] nums = new int[n];
-		for(int i = 0 ;i < n; i++) {
-			nums[i] = sc.nextInt();
-		}
-		int answer = 0;
-		for(int i = 0 ; i<nums.length;i++) {
-			if(chk(nums[i])) {
-				answer++;
-			}
-		}
-		System.out.println(answer);
-	}
-	static boolean chk(int num) {
-		if(num == 0 || num == 1) return false;
-		int cnt = 0;
-		for(int i = 2; i<=num; i++) {
-			if(num%i== 0) {
-				cnt++;
-			}
-		}
-		if(cnt == 1) {
-			return true;
-		}
-		return false;
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int a = Integer.parseInt(st.nextToken());
+        int count = 0;
+        int answer = 0;
+        st = new StringTokenizer(br.readLine()," ");
+        for(int k = 0 ;k < a; k++) {
+            count = 0;
+            int b = Integer.parseInt(st.nextToken());
+            for(int i = 2 ;i <=b; i++){
+                if(b%i == 0){
+                    count++;
+                }
+            }
+            if(count == 1){
+                answer++;
+            }
+        }
+        System.out.println(answer);
+
+    }
+
 }
